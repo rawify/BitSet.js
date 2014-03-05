508eeb1e86669fbaa5a486f10480c478eadafe0a
roberteisele <roberteisele@192.168.1.5>
2014-03-05 14:17:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,140 @@
+# BitSet.js 
+
+BitSet.js is a [Bit-Array](http://en.wikipedia.org/wiki/Bit_array) implementation in JavaScript.
+
+With this library, you can work on large bit vectors without worring about system constraints, given by integer size.
+
+Examples
+===
+
+Basic usage
+---
+```javascript
+var bs = new BitSet(500); // We need 500 bits
+bs.set(128, 1); // Set bit at position 128
+```
+
+Range Set
+---
+```javascript
+var bs = new BitSet; // Default is 31 bit
+bs.setRange(10, 18, 1); // Set a 1 between 10 and 18
+```
+
+
+Flipping bits
+---
+```javascript
+var bs = new BitSet(40);
+bs.flip();
+bs.flip(29, 35);
+var str = bs.toString("-"); // Separator every 31 bits is the dash "-"
+
+if (str === "1111111111111111111111111100000-0011111111111111111111111111111") {
+   console.log("YES!");
+}
+```
+
+Installation
+===
+
+```
+npm install bitset.js
+```
+
+Available methods
+===
+
+Bitwise functions
+---
+***Bitwise not: ***
+```
+BitSet not(BitSet x)
+```
+***Bitwise and: ***
+```
+BitSet and(BitSet x)
+```
+***Bitwise or: ***
+```
+BitSet or(BitSet x)
+```
+***Bitwise nand: ***
+```
+BitSet nand(BitSet x)
+```
+***Bitwise nor: ***
+```
+BitSet nor(BitSet x)
+```
+***Bitwise xor: ***
+```
+BitSet xor(BitSet x)
+```
+***Set a bit at position index, default 1: ***
+```
+BitSet set(int index, int value=1)
+```
+***Get a bit at position index: ***
+```
+BitSet get(int index)
+```
+***Set a range of bits, either by a binary string or by a single bit value ***
+```
+BitSet setRange(int start, int end, String binstr)
+BitSet setRange(int start, int end, int value)
+```
+*** Retrieve a range of bits, indicated by start and end index ***
+```
+BitSet getRange(int start, int end)
+```
+***Get the number of bits set: ***
+```
+int cardinality()
+```
+***Get the most significant bit set, same as log base two: ***
+```
+int msb()
+```
+***Clear a range of bits, either all, a certain position or indicated with start and end***
+```
+BitSet clear()
+BitSet clear(int pos)
+BitSet clear(int start, int end)
+```
+
+*** Invert/Flip either all bits, a single bit position or a range of bits***
+```
+BitSet flip()
+BitSet flip(int pos)
+BitSet flip(int start, int end)
+```
+
+Comparision functions
+---
+***Compare (=same size and all bits equal) two BitSet objects ***
+```
+boolean equals(BitSet x)
+```
+***Check if all bits of a BitSet are set to 0***
+```
+boolean isEmpty()
+```
+
+Misc functions
+---
+***Overrides the toString function for a pretty representation ***
+```
+String toString(String separator="")
+```
+*** Create a 100% copy of the actual BitSet object ***
+```
+BitSet clone()
+```
+
+
+Note
+===
+The allocated size is always a power of two minus 1! If you need 500 bits, the actual size allocated is 527 (= ceil(500 / 31)). All the additional bits are usable of course.
+
+If you want to extend the library, please provide test cases in your commit.
---
+++ tests/bitset.test.js
@@ -0,0 +1,314 @@
+
+var assert = require('assert');
+
+var BitSet = require('../bitset.min').BitSet;
+
+function i2s(arr) {
+
+    var str = "{ ";
+
+    for (var i = 0; i < arr.length; i++) {
+        if (i > 0)
+            str+= ", ";
+        str+= arr[i];
+    }
+    return str + " }";
+}
+
+describe('BitSet', function(){
+
+    it('Small element construct', function(){
+
+        var bs = new BitSet(2);
+        assert.equal(bs.size, 31);
+        assert.equal(i2s(bs), '{ 0 }');
+    });
+
+    it('32s construcst', function(){
+
+        var bs = new BitSet(32);
+        assert.equal(bs.size, 62);
+        assert.equal(i2s(bs), '{ 0, 0 }');
+    });
+
+    it('500s construct INT', function(){
+
+        var bs = new BitSet(500);
+        assert.equal(bs.size, 527);
+        assert.equal(i2s(bs), '{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }');
+    });
+
+    it('not', function(){
+
+        var bs1 = new BitSet(64);
+
+        bs1[1] = 5;
+
+        bs1.not();
+
+        assert.equal(bs1.size, 93);
+        assert.equal(i2s(bs1), '{ -1, -6, -1 }');
+    });
+
+    it('and', function(){
+
+        var bs1 = new BitSet(64);
+        var bs2 = new BitSet(64);
+
+        bs1[1] = 5;
+        bs2[1] = 1;
+
+        bs1.and(bs2);
+
+        assert.equal(bs1.size, 93);
+        assert.equal(i2s(bs1), '{ 0, 1, 0 }');
+    });
+
+    it('or', function(){
+
+        var bs1 = new BitSet(64);
+        var bs2 = new BitSet(64);
+
+        bs1[1] = 5;
+        bs2[1] = 8;
+
+        bs1.or(bs2);
+
+        assert.equal(bs1.size, 93);
+        assert.equal(i2s(bs1), '{ 0, 13, 0 }');
+    });
+
+    it('nand', function(){
+
+        var bs1 = new BitSet(64);
+        var bs2 = new BitSet(64);
+
+        bs1[1] = 5;
+        bs2[1] = 1;
+
+        bs1.nand(bs2);
+
+        assert.equal(bs1.size, 93);
+        assert.equal(i2s(bs1), '{ -1, -2, -1 }');
+    });
+
+    it('nor', function(){
+
+        var bs1 = new BitSet(64);
+        var bs2 = new BitSet(64);
+
+        bs1[1] = 5;
+        bs2[1] = 8;
+
+        bs1.nor(bs2);
+
+        assert.equal(bs1.size, 93);
+        assert.equal(i2s(bs1), '{ -1, -14, -1 }');
+    });
+
+    it('xor', function(){
+
+        var bs1 = new BitSet(64);
+        var bs2 = new BitSet(64);
+
+        bs1[1] = 22;
+        bs2[1] = 9;
+
+        bs1.xor(bs2);
+
+        assert.equal(bs1.size, 93);
+        assert.equal(i2s(bs1), '{ 0, 31, 0 }');
+    });
+
+    it('equals', function(){
+
+        var bs1 = new BitSet(64);
+        var bs2 = new BitSet(64);
+
+        bs1[1] = 63;
+        bs2[1] = 63;
+
+        assert.equal(bs1.equals(bs2), bs2.equals(bs1));
+        assert.equal(bs1.equals(bs2), true);
+    });
+
+    it('clone', function(){
+
+        var bs1 = new BitSet(64);
+        bs1[1] = 63;
+
+        var bs2 = bs1.clone();
+
+        assert.equal(bs1.equals(bs2), bs2.equals(bs1));
+        assert.equal(bs1.equals(bs2), bs1.size == bs2.size);
+    });
+
+    it('isEmpty', function(){
+
+        var bs1 = new BitSet(500);
+        var bs2 = new BitSet(500);
+
+        bs2[15] = 666;
+
+        assert.equal(bs1.isEmpty(bs2), !bs2.isEmpty(bs1));
+    });
+
+    it('toString', function(){
+
+        var ex = "0000000000000000000000001111100-0000000000000000000001010011010";
+
+        var bs = new BitSet(40);
+
+        bs[0] = 666;
+        bs[1] = 31 << 2;
+
+        assert.equal(bs.size, 62);
+        assert.equal(bs.toString("-"), ex);
+        assert.equal("" + bs, ex.replace("-", ""));
+    });
+
+    it('cardinality', function(){
+
+        var bs = new BitSet(40);
+
+        bs[0] = 15 << 3;
+        bs[1] = 31 << 2;
+
+        assert.equal(bs.cardinality(), 9);
+    });
+
+    it('msb', function(){
+
+        var bs = new BitSet(40);
+
+        bs[0] = 15 << 3;
+        bs[1] = 31 << 2;
+
+        assert.equal(bs.msb(), 37);
+    });
+
+    it('msb big', function(){
+
+        var bs = new BitSet(500);
+
+        bs.set(332);
+        bs.set(333);
+
+        assert.equal(bs.msb(), 333);
+    });
+
+    it('set', function(){
+
+        var bs = new BitSet(40);
+
+        bs.set(4, 1); // Set bit on 4th pos
+        bs.set(0); // Set bit on 0th pos
+        bs.set(22, 1);
+
+        bs.set(33, 1); // Set
+
+        assert.equal(bs.toString("!"), '0000000000000000000000000000100!0000000010000000000000000010001');
+
+        bs.set(33, 0); // And reset
+        assert.equal(bs.toString("!"), '0000000000000000000000000000000!0000000010000000000000000010001');
+
+
+        bs.set(330, 1); // Will be ignored, out of rage
+
+        assert.equal(bs.msb(), 22); // Thus, msb is on 22
+    });
+
+    it('get', function(){
+
+        var bs = new BitSet();
+
+        bs.set(4, 1); // Set bit on 4th pos
+        bs.set(0); // Set bit on 0th pos
+
+        assert.equal(bs.get(4) + bs.get(0), 2);
+    });
+
+
+    it('getRange', function(){
+
+        var bs = new BitSet(100);
+        var ex = '10011001000100110011001100110011';
+
+        bs[0] = 0x9999999;
+        bs[1] = 0x9999999;
+
+        assert.equal(ex, bs.getRange(7, 38).toString().substr(-ex.length));
+    });
+
+    it('setRange 1', function(){
+
+        var bs = new BitSet(30);
+
+        bs.setRange(3, 10);
+
+        assert.equal(bs.toString("..."), "0000000000000000000011111111000");
+    });
+
+    it('setRange 2', function(){
+
+        var bs = new BitSet(62);
+
+        bs.setRange(0, 61);
+        bs.setRange(30, 45, 0);
+
+        assert.equal(bs.toString("!"), "1111111111111111000000000000000!0111111111111111111111111111111");
+    });
+
+    it('setRange 3', function(){
+
+        var bs = new BitSet(30);
+
+        bs.setRange(2, 12, "1111001111");
+
+        assert.equal(bs.toString("!"), "0000000000000000000111100111100");
+    });
+
+    it('flip/clear 1', function(){
+
+        var bs = new BitSet(40);
+
+        bs.flip();
+        assert.equal(bs.toString("-"), "1111111111111111111111111111111-1111111111111111111111111111111");
+
+    });
+
+    it('flip/clear 2', function(){
+
+        var bs = new BitSet(40);
+
+        bs.flip();
+
+        bs.flip(29, 35);
+        assert.equal(bs.toString("-"), "1111111111111111111111111100000-0011111111111111111111111111111");
+
+    });
+
+    it('flip/clear 3', function(){
+
+        var bs = new BitSet(40);
+
+        bs.flip();
+
+        bs.flip(29, 35);
+
+        bs.clear(50, 55);
+        assert.equal(bs.toString("-"), "1111110000001111111111111100000-0011111111111111111111111111111");
+
+    });
+    
+    it('flip range check', function(){
+
+        var bs = new BitSet(40);
+        
+        assert.equal(bs.flip(-1, 0), null);
+        assert.equal(bs.flip(0, 64), null);
+        assert.equal(bs.flip(1, 0), null);
+
+    });
+
+});
