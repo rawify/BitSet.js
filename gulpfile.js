8feb8b9b9d5d50a895abb7e314002891f881e0ee
Robert Eisele <robert@xarg.org>
2018-11-16 16:58:27 +0100

    v++

--- gulpfile.js
+++ gulpfile.js
@@ -18,15 +18,15 @@ var closureCompiler = require('google-closure-compiler').gulp();
 
 gulp.task('compile', function() {
   return gulp.src(['bitset.js'])
-      .pipe(closureCompiler({
-        compilation_level: 'ADVANCED',
-        externs: 'moduleExterns.js',
-        language_in: 'ES6_STRICT',
-        language_out: 'ES5_STRICT',
-        js_output_file: 'bitset.min.js',
-        warning_level: 'VERBOSE'
-      }))
-      .pipe(gulp.dest('.'));
+          .pipe(closureCompiler({
+            compilation_level: 'ADVANCED',
+            externs: 'moduleExterns.js',
+            language_in: 'ES6_STRICT',
+            language_out: 'ES5_STRICT',
+            js_output_file: 'bitset.min.js',
+            warning_level: 'VERBOSE'
+          }))
+          .pipe(gulp.dest('.'));
 });
 
 gulp.task('default', ['compile']);
